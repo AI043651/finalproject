f2502ae1029e8ff19cb951f27755e9430aeb3aef
AI043651 <AI043651@au.edu.tw>
2019-05-17 16:01:13 +0800

    menucase

--- finalproject.c
+++ finalproject.c
@@ -6,14 +6,42 @@ struct Friendslist{
 	char adderass[100];
 }
 int menu(void);
-void creadfreindtable(void);
+void creationfriendtable(void);
 void findfreind(void);
 void insertremove(void);
+
 int main(void){
 	struct Freindslist;
-	int phone;
-	char name[30];
-	char adderass[100];
+	FLIE *fPtr;
+	fptr=fopen("FriendList.txt","rb+");
+	if(fptr=NULL){
+		while((ans==menu())!=5){
+			switch(ans){
+				case 1:
+					creationfriendtble(fptr);
+					system("CLS");
+					break;
+				case 2:
+					findfreind(fptr);
+					system("CLS");
+					break;
+				case 3:
+					insertmove(fptr);
+					system("CLS");
+					break;
+				default:
+					printf("Incorrect choice\n");
+					system("CLS");
+					break;
 
+			}
+		}
+		fclose(fptr);
+	}
+	else{
+		printf("ERROR!\n");
 
+	}
+	system("pause");
+	return 0;
 }
