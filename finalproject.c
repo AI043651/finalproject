ac6f9aa31997f19514cf53a8b1ba404a7640a553
AI043651 <AI043651@au.edu.tw>
2019-05-17 15:31:15 +0800

    menu

---
+++ finalproject.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Friendslist{
+	int phone;
+	char name[30];
+	char adderass[100];
+}
+int menu(void);
+void creadfreindtable(void);
+void findfreind(void);
+void insertremove(void);
+int main(void){
+	struct Freindslist;
+	int phone;
+	char name[30];
+	char adderass[100];
+
+
+}
